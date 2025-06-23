281be251194c61e6f7f956d0ebb47c54c9103384
Vijay Ganesh <vijay.ganesh0811@gmail.com>
2025-06-23 21:32:00 +0530

    Eureka Client Enablement

--- src/main/java/org/springboot/questionservice/QuestionServiceApplication.java
+++ src/main/java/org/springboot/questionservice/QuestionServiceApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class QuestionServiceApplication {
 
+    /*
+     * application.properties has settings for enabling
+     * Eureka Client, so other registered MicroServices
+     * Can access the required methods through OpenFeign
+     */
     public static void main(String[] args) {
         SpringApplication.run(QuestionServiceApplication.class, args);
     }
